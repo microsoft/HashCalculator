0c58714ca1da3d58ed42e8782a15a60c565a89bc
Ronald Aigner <raigner@microsoft.com>
2016-03-31 10:50:07 -0700

    Hash calculator universal app

---
+++ HashCalculator/Worker.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+
+namespace HashCalculator
+{
+    class Worker
+    {
+        public static string[] GetHashingAlgorithms()
+        {
+            return new[] {
+                HashAlgorithmNames.Md5,
+                HashAlgorithmNames.Sha1,
+                HashAlgorithmNames.Sha256,
+                HashAlgorithmNames.Sha384,
+                HashAlgorithmNames.Sha512
+                };
+        }
+
+        public static byte[] ComputeHash(string algorithm, byte[] input)
+        {
+            HashAlgorithmProvider sha = HashAlgorithmProvider.OpenAlgorithm(algorithm);
+
+            var output = sha.HashData(CryptographicBuffer.CreateFromByteArray(input));
+
+            byte [] ret;
+            CryptographicBuffer.CopyToByteArray(output, out ret);
+
+            return ret;
+        }
+
+        public static string ComputeHash(string algorithm, string input, bool isByteArray)
+        {
+            byte[] a;
+            if (isByteArray)
+            {
+                // convert from characters to values ('0' -> 0, etc.)
+                var inBuffer = CryptographicBuffer.DecodeFromHexString(input);
+                CryptographicBuffer.CopyToByteArray(inBuffer, out a);
+            }
+            else
+            {
+                int i = 0;
+                a = new byte[input.Length];
+                foreach (char c in input)
+                {
+                    a[i++] = (byte)c;
+                }
+            }
+
+            if (a.Length == 0)
+                return "";
+
+            byte[] encoded = ComputeHash(algorithm, a);
+            if (encoded == null)
+                return "";
+
+            var buffer = CryptographicBuffer.CreateFromByteArray(encoded);
+            var ret = CryptographicBuffer.EncodeToHexString(buffer);
+
+            return ret;
+        }
+    }
+}
